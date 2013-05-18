95865e44eac5b454a59b1c37f359af66fb6a3067
Riad KRIM <twiterzx@yahoo.fr>
2013-05-18 11:22:06 +0200

    [GIT]				Xcode init

---
+++ ArenaBlock/ArenaBlock/Classes/MainController.h
@@ -0,0 +1,13 @@
+//
+//  MainController.h
+//  ArenaBlock
+//
+//  Created by Riad KRIM on 18/05/13.
+//  Copyright (c) 2013 beMyApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainController : UIViewController
+
+@end
---
+++ ArenaBlock/ArenaBlock/Classes/MainController.m
@@ -0,0 +1,25 @@
+//
+//  MainController.m
+//  ArenaBlock
+//
+//  Created by Riad KRIM on 18/05/13.
+//  Copyright (c) 2013 beMyApp. All rights reserved.
+//
+
+#import "MainController.h"
+
+@interface MainController ()
+
+@end
+
+@implementation MainController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
