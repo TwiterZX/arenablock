fa7176de9c5e000e9a105b7cc9f479dd16b8a22d
Riad KRIM <twiterzx@yahoo.fr>
2013-05-18 13:24:36 +0200

    [FIX]				StoryBoard:	fix classname for main view

---
+++ ArenaBlock/ArenaBlock/Classes/GameCenterManager.h
@@ -0,0 +1,13 @@
+//
+//  GameCenterManager.h
+//  ArenaBlock
+//
+//  Created by Riad KRIM on 18/05/13.
+//  Copyright (c) 2013 beMyApp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameCenterManager : NSObject
+
+@end
---
+++ ArenaBlock/ArenaBlock/Classes/GameCenterManager.m
@@ -0,0 +1,13 @@
+//
+//  GameCenterManager.m
+//  ArenaBlock
+//
+//  Created by Riad KRIM on 18/05/13.
+//  Copyright (c) 2013 beMyApp. All rights reserved.
+//
+
+#import "GameCenterManager.h"
+
+@implementation GameCenterManager
+
+@end
